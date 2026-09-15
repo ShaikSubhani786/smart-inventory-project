e7cf2dffe161276c345e28f26746e09e8af35cb1
shaik.subhani <shaik.subbu384@gmail.com>
2026-09-15 14:37:57 +0530

    Clean obsolete backend test scripts and configure pytest

--- backend/test_db_dependency.py
+++
@@ -1,9 +0,0 @@
-from app.api.dependencies import get_db
-
-generator = get_db()
-
-db = next(generator)
-
-print(db)
-
-generator.close()
--- backend/test_schema.py
+++
@@ -1,9 +0,0 @@
-from app.schemas.user import UserCreate
-
-user = UserCreate(
-    username="Subhani",
-    email="abc",
-    password="Password@123"
-)
-
-print(user)
